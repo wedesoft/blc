51dc3764c7d9629b026afef2f18a22796f070a53
Jan Wedekind <jan@wedesoft.de>
2012-09-27 13:34:16 +0100

    Testing preservation of symbol.

--- mini_spec.sh
+++ mini_spec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RETVAL=0
-if test "`echo 'sym' | ./mini`" != "sym"; then
-  echo "'mini' does not preserve symbol"
+if test "`echo \'symbol | ./mini`" != "'symbol"; then
+  echo "'mini' does not preserve symbols"
   RETVAL=1
 fi
 exit $RETVAL
