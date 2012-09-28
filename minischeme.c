3b38ef6bd8838055e3b7292bfb5ac97cd0f9300a
Jan Wedekind <jan@wedesoft.de>
2012-09-29 00:17:12 +0100

    Implemented reading and writing of s-expressions.

--- minischeme.c
+++ minischeme.c
@@ -14,9 +14,30 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define PAIRS 128
 #define TOKENSIZE 8
+#define NIL -1
+
+typedef enum {
+  PAIR = 0,
+  SYMBOL
+} type_t;
+
+typedef struct {
+  int car;
+  int cdr;
+} pair_t;
+
+typedef struct {
+  type_t type;
+  union {
+    pair_t pair;
+    char symbol[TOKENSIZE + 3];
+  };
+} cell_t;
 
 char *read_token(char *str)
 {
@@ -25,11 +46,11 @@ char *read_token(char *str)
     int len = strlen(retval) - 1;
     retval[len] = '\0';
     if (len <= 0) {
-      fprintf(stderr, "Error: Encountered empty token.\n");
+      fprintf(stderr, "Error: Encountered empty token\n");
       exit(1);
     };
     if (len > TOKENSIZE) {
-      fprintf(stderr, "Error: Token %s... longer than %d characters.\n",
+      fprintf(stderr, "Error: Token %s... longer than %d characters\n",
               retval, TOKENSIZE);
       exit(1);
     };
@@ -37,13 +58,75 @@ char *read_token(char *str)
   return retval;
 }
 
+int n = 0;
+cell_t cells[PAIRS];
+
+int parse_list(void)
+{
+  int retval;
+  int cell = parse();
+  if (cell != NIL) {
+    retval = n++;
+    cells[retval].type = PAIR;
+    cells[retval].pair.car = cell;
+    cells[retval].pair.cdr = parse_list();
+  } else {
+    retval = -1;
+  };
+  return retval;
+}
+
+int parse(void)
+{
+  int retval = n;
+  char *str = read_token(cells[retval].symbol);
+  if (!str) {
+    fprintf(stderr, "Error: Unexpected end of input\n");
+    exit(1);
+  };
+  if (strcmp(str, "(") == 0) {
+    n++;
+    cells[retval].type = PAIR;
+    cells[retval].pair.car = parse_list();
+    cells[retval].pair.cdr = NIL;
+  } else if (strcmp(str, ")") == 0) {
+    retval = -1;
+  } else {
+    n++;
+    cells[retval].type = SYMBOL;
+  };
+  return retval;
+}
+
+void print(int i);
+
+void print_list(int i)
+{
+  if (cells[i].type == PAIR) {
+    print(cells[i].pair.car);
+    if (cells[i].pair.cdr != NIL) {
+      fputc(' ', stdout);
+      print_list(cells[i].pair.cdr);
+    }
+  };
+}
+
+void print(int i)
+{
+  if (cells[i].type == PAIR) {
+    fputc('(', stdout);
+    print_list(cells[i].pair.car);
+    fputc(')', stdout);
+  } else {
+    char *symbol = cells[i].symbol;
+    while (*symbol) fputc(*symbol++, stdout);
+  }
+}
+
 int main(void)
 {
-  while (1) {
-    char line[TOKENSIZE + 3];
-    char *str = read_token(line);
-    if (!str) break;
-    printf("%s\n", str);
-  };
+  print(parse());
+  fputc('\n', stdout);
   return 0;
 }
+
