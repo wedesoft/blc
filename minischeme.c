e9078c677e6da726c07996f2f69f28f42b8ec420
Jan Wedekind <jan@wedesoft.de>
2012-10-01 22:52:40 +0100

    Introduced 'car', and 'cdr'.

--- minischeme.c
+++ minischeme.c
@@ -41,6 +41,14 @@ typedef struct {
   };
 } cell_t;
 
+int n_cells = 0;
+cell_t cells[MAX_PAIRS];
+
+char *symbol(int i)
+{
+  return cells[i].symbol;
+}
+
 char *read_token(char *str)
 {
   int len = 0;
@@ -67,22 +75,9 @@ char *read_token(char *str)
   return retval;
 }
 
-int n_cells = 0;
-cell_t cells[MAX_PAIRS];
-
-int read_list(void)
+type_t type(int i)
 {
-  int retval;
-  int cell = read_expression();
-  if (cell != NIL) {
-    retval = n_cells++;
-    cells[retval].type = PAIR;
-    cells[retval].pair.car = cell;
-    cells[retval].pair.cdr = read_list();
-  } else {
-    retval = NIL;
-  };
-  return retval;
+  return cells[i].type;
 }
 
 void make_pair(int cell, int car, int cdr)
@@ -92,10 +87,23 @@ void make_pair(int cell, int car, int cdr)
   cells[cell].pair.cdr = cdr;
 }
 
+int read_list(void)
+{
+  int retval;
+  int cell = read_expression();
+  if (cell != NIL) {
+    retval = n_cells++;
+    make_pair(retval, cell, read_list());
+  } else {
+    retval = NIL;
+  };
+  return retval;
+}
+
 int read_expression(void)
 {
   int retval = n_cells;
-  char *str = read_token(cells[retval].symbol);
+  char *str = read_token(symbol(retval));
   if (str) {
     switch (str[0]) {
     case '(':
@@ -117,13 +125,14 @@ void print_expression(int i);
 
 void print_list(int i)
 {
-  if (cells[i].type == PAIR) {
-    print_expression(cells[i].pair.car);
-    if (cells[i].pair.cdr != NIL) {
+  if (type(i) == PAIR) {
+    print_expression(car(i));
+    if (cdr(i) != NIL) {
       fputc(' ', stdout);
-      print_list(cells[i].pair.cdr);
+      print_list(cdr(i));
     }
-  };
+  } else
+    print_expression(i);
 }
 
 void print_expression(int i)
@@ -131,21 +140,55 @@ void print_expression(int i)
   if (i == NIL) {
     fputc('(', stdout);
     fputc(')', stdout);
-  } else if (cells[i].type == PAIR) {
+  } else if (type(i) == PAIR) {
     fputc('(', stdout);
     print_list(i);
     fputc(')', stdout);
   } else {
-    char *symbol = cells[i].symbol;
-    while (*symbol) fputc(*symbol++, stdout);
+    char *p = symbol(i);
+    while (*p) fputc(*p++, stdout);
   }
 }
 
-int eval(int i)
+int car(int i)
+{
+  if (type(i) != PAIR) {
+    fprintf(stderr, "Argument to 'car' must be a pair\n");
+    exit(1);
+  };
+  return cells[i].pair.car;
+}
+
+int cdr(int i)
+{
+  if (type(i) != PAIR) {
+    fprintf(stderr, "Argument to 'cdr' must be a pair\n");
+    exit(1);
+  };
+  return cells[i].pair.cdr;
+}
+
+int eval_list(int i)
 {
   int retval;
-  if (cells[i].type == PAIR) {
+  if (type(car(i)) == SYMBOL) {
+    char *p = symbol(car(i));
+    if (strcmp(p, "car") == 0)
+      retval = car(car(cdr(i)));
+    else if (strcmp(p, "cdr") == 0)
+      retval = cdr(car(cdr(i)));
+    else
+      retval = i;
+  } else
     retval = i;
+  return retval;
+}
+
+int eval_expression(int i)
+{
+  int retval;
+  if (type(i) == PAIR) {
+    retval = eval_list(i);
     // retval = cells[cells[i].pair.cdr].pair.car;
     //
     // pair (not atom)
@@ -188,15 +231,15 @@ int main(void)
       else
         fprintf(stderr, "   ");
       fprintf(stderr, "%2d: ", i);
-      if (cells[i].type == PAIR)
-        fprintf(stderr, "car = %2d, cdr = %2d\n", cells[i].pair.car, cells[i].pair.cdr);
+      if (type(i) == PAIR)
+        fprintf(stderr, "car = %2d, cdr = %2d\n", car(i), cdr(i));
       else
-        fprintf(stderr, "symbol = %s\n", cells[i].symbol);
+        fprintf(stderr, "symbol = %s\n", symbol(i));
     };
     fputc('\n', stderr);
 #endif
     if (expr == NIL) break;
-    print_expression(eval(expr)); fprintf(stdout, "\n");
+    print_expression(eval_expression(expr)); fprintf(stdout, "\n");
     // print_expression(expr); fprintf(stdout, "\n");
   };
   return 0;
