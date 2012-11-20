e20c58df8ee40a7c27e6a20722fe1da7f1e5db64
Jan Wedekind <j.wedekind@digital-science.com>
2012-11-20 18:10:17 +0000

    Started 'blc.c'

---
+++ blc.c
@@ -0,0 +1,22 @@
+/* Bracket - Attempt at writing a small Racket (Scheme) interpreter
+ * Copyright (C) 2012  Jan Wedekind
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+int main(void)
+{
+  // Virtual machine for binary lambda calculus
+  return 0;
+}
+
