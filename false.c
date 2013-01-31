31f54f70a8c974a0ec8d3efb0c1c6574a4bf4dc1
Jan Wedekind <j.wedekind@digital-science.com>
2013-01-31 12:56:00 +0000

    Added 'true', 'false', and 'nil'

---
+++ false.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  fputs("000010", stdout);
+  return 0;
+}
+
---
+++ true.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  fputs("0000110", stdout);
+  return 0;
+}
+
