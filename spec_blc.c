aee8d11b3e7c7598518a146ea954ce9208da9650
Jan Wedekind <jan@wedesoft.de>
2013-03-22 08:31:39 +0000

    Documentation

--- spec_blc.c
+++ spec_blc.c
@@ -141,9 +141,9 @@ int main(void)
   retval = retval | test_input("01 10 0000110", "#<proc:10;#env=2>");// end of string
   retval = retval | test_input("01 10 0000110 0", "#<proc:0010;#env=2>");// read '0'
   retval = retval | test_input("01 10 0000110 1", "#<proc:00110;#env=2>");// read '1'
-  retval = retval | test_input("01 01 10 000000000010 0000110 0", "#<proc:0010;#env=4>");// no EOS
-  retval = retval | test_input("01 01 10 000000000010 0000110 1", "#<proc:0010;#env=4>");// no EOS
-  retval = retval | test_input("01 01 10 000000000010 0000110", "#<proc:00110;#env=1>");// EOS
+  retval = retval | test_input("01 01 10 000000000010 0000110 0", "#<proc:0010;#env=4>");// determine no EOS
+  retval = retval | test_input("01 01 10 000000000010 0000110 1", "#<proc:0010;#env=4>");// determine no EOS
+  retval = retval | test_input("01 01 10 000000000010 0000110", "#<proc:00110;#env=1>");// determine EOS
   retval = retval | test_input("01 01 10 000010 0000110 00", "#<proc:0010;#env=3>");
   retval = retval | test_input("01 01 10 000010 0000110 01", "#<proc:00110;#env=3>");
   retval = retval | test_input("01 01 01 10 0000110 1110 110 0", "10");// select variable using input bit
