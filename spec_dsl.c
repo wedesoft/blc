ccb4fe84259c71a5d8b29a8cd52eae6f2607d79f
Jan Wedekind <jan@wedesoft.de>
2013-09-23 13:44:47 +0100

    Added multiplication

--- spec_dsl.c
+++ spec_dsl.c
@@ -18,34 +18,52 @@
 #include <string.h>
 #include "blc.h"
 
-int first_bit_;
-int first_bit(int list)
+int odd_;
+int odd(int list)
 {
-  return call(first_bit_, list);
+  return call(odd_, list);
 }
 
-int rest_bits_;
-int rest_bits(int list)
+int shr_;
+int shr(int list)
 {
-  return call(rest_bits_, list);
+  return call(shr_, list);
 }
 
+int shl_;
+int shl(int list)
+{
+  return call(shl_, list);
+}
+
+int plus_;
 int plus(int a, int b)
 {
-  int fun = y_comb(lambda(lambda(lambda(op_if(op_and(empty(var(0)), empty(var(1))), op_if(var(2), pair(t(), f()), f()),
-                              pair(op_xor(op_xor(first_bit(var(0)), first_bit(var(1))), var(2)),
-                                   call(call(call(var(3),
-                                                  op_if(var(2),
-                                                        op_or(first_bit(var(0)), first_bit(var(1))),
-                                                        op_and(first_bit(var(0)), first_bit(var(1))))),
-                                             rest_bits(var(1))), rest_bits(var(0)))))))));
-  return call(call(call(fun, f()), b), a);
+  return call(call(call(plus_, f()), b), a);
+}
+
+int mult_;
+int mult(int a, int b)
+{
+  return call(call(mult_, b), a);
 }
 
 void init_lib(void)
 {
-  first_bit_ = lambda(op_if(empty(var(0)), f(), first(var(0))));
-  rest_bits_ = lambda(op_if(empty(var(0)), f(), rest(var(0))));
+  odd_ = lambda(op_if(empty(var(0)), f(), first(var(0))));
+  shr_ = lambda(op_if(empty(var(0)), f(), rest(var(0))));
+  shl_ = lambda(op_if(empty(var(0)), f(), pair(f(), var(0))));
+  plus_ = y_comb(lambda(lambda(lambda(op_if(op_and(empty(var(0)), empty(var(1))), op_if(var(2), pair(t(), f()), f()),
+                    pair(op_xor(op_xor(odd(var(0)), odd(var(1))), var(2)),
+                         call(call(call(var(3),
+                                        op_if(var(2),
+                                              op_or(odd(var(0)), odd(var(1))),
+                                              op_and(odd(var(0)), odd(var(1))))),
+                                   shr(var(1))), shr(var(0)))))))));
+  mult_ = y_comb(lambda(lambda(op_if(empty(var(0)),
+                                     f(),
+                                     call(lambda(op_if(first(var(1)), plus(var(2), var(0)), var(0))),
+                                          shl(call(call(var(2), shr(var(0))), var(1))))))));
 }
 
 int main(void)
@@ -53,10 +71,25 @@ int main(void)
   init();
   init_lib();
   int n = cell(VAR);
+  // Check for even/odd
+  assert(is_f(odd(int_to_num(4))));
+  assert(!is_f(odd(int_to_num(5))));
+  // Shift right
+  assert(num_to_int(shr(int_to_num(6))) == 3);
+  assert(num_to_int(shr(int_to_num(5))) == 2);
+  // Shift left
+  assert(num_to_int(shl(int_to_num(6))) == 12);
+  assert(num_to_int(shl(int_to_num(5))) == 10);
   // Integer addition
+  assert(num_to_int(plus(int_to_num(0), int_to_num(1))) == 1);
   assert(num_to_int(plus(int_to_num(2), int_to_num(1))) == 3);
   assert(num_to_int(plus(int_to_num(2), int_to_num(2))) == 4);
   assert(num_to_int(plus(int_to_num(1), int_to_num(3))) == 4);
+  // Integer multiplication
+  assert(num_to_int(mult(int_to_num(0), int_to_num(2))) == 0);
+  assert(num_to_int(mult(int_to_num(4), int_to_num(1))) == 4);
+  assert(num_to_int(mult(int_to_num(4), int_to_num(2))) == 8);
+  assert(num_to_int(mult(int_to_num(5), int_to_num(3))) == 15);
   fprintf(stderr, "Test suite requires %d cells.\n", cell(VAR) - n - 1);
   destroy();
   return 0;
